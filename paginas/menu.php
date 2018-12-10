2edfc50715553332053bdfb5fdbf84f8d9a8549f
Alim910 <44564681+Alim910@users.noreply.github.com>
2018-12-09 21:13:02 -0600

    Update menu.php

--- paginas/menu.php
+++ paginas/menu.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -5,6 +9,7 @@
 <!--[if gt IE 8]><!--> 
 <html class="no-js"> <!--<![endif]-->
 <head>
+
     <title>Polenta</title>
 
     <!-- meta -->
@@ -14,9 +19,13 @@
 
     <!-- css -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="dist/bootstrap-clockpicker.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/github.min.css">
+
+
 
     <!-- google font -->
     <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Kreon:300,400,700'>
@@ -25,6 +34,8 @@
     <script type="text/javascript" src="extras/jquery.min.1.7.js"></script>
 <script type="text/javascript" src="extras/modernizr.2.5.3.min.js"></script>
 <script type="text/javascript" src="lib/hash.js"></script>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="dist/bootstrap-clockpicker.min.js"></script>
 
     <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 </head>
@@ -42,7 +53,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><h2>Meat King</h2></a>
+                <a class="navbar-brand" href="#"><h2>Polenta</h2></a>
             </div><!-- navbar-header -->
         <div id="navbar" class="navbar-collapse collapse">
             <div class="hidden-xs" id="logo"><a href="#header">
@@ -57,7 +68,43 @@
                 
                 <li><a href="?op=calidad">CALIDAD</a></li>
                 <li><a href="?op=publicidad">PUBLICIDAD</a></li>
-                <li><a href="?op=login">INICIAR SESION</a></li>
+                
+          
+  
+                            <?php
+                                if(isset($_SESSION['activo'])){
+                                    if($_SESSION['activo'] == 1){ ?>
+                                        <div class="dropdown">
+                                      <li><a href="?op=login"> . <?php echo $_SESSION['USUARIO'];?> . </a></li>
+                                                  <div class="dropdown-content">
+                                                      <p class="text-left"><strong><?php echo $_SESSION['NOMBRE'].' '.$_SESSION['AP'];?></strong></p>
+                                                       <p class="text-left small"><?php echo $_SESSION['CORREO'];?></p>
+                                                  <a href="?op=perfil">Actualizar Datos</a>
+                                                  <a href="?op=logout">Cerrar Sesion</a>
+                                                 
+                                                  </div>
+                                                     </div>
+                                        
+                                    <?php 
+                                    }
+                                }else{?>
+                                     <li><a href="?op=login">INICIAR SESION</a></li>
+                                <?php
+                                }
+                            ?>
+  
+  
+  
+
+
+</div>
+                
+                
+                
+                
+              
+                
+                
 
                 
                 <!--fix for scroll spy active menu element-->
@@ -68,3 +115,4 @@
         </div><!-- container -->
     </div><!-- menu -->
 </header>
+
